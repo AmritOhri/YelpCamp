36cd16d89073b173285c8857faa64115d3251555
AmritOhri <amritohri2000@gmail.com>
2020-09-25 12:57:14 +0000

    With env variables for goorm and heroku setup

--- app.js
+++ app.js
@@ -17,35 +17,12 @@ var commentRoutes = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	indexRoutes = require("./routes/index");
 
-// mongoose.connect('mongodb://localhost:27017/YelpCamp',{
-mongoose.connect("mongodb+srv://AmritOhri:abcdefgh12345678@cluster0.8r7sj.mongodb.net/YelpCamp?retryWrites=true&w=majority", {
+mongoose.connect(process.env.DATABASEURL,{
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 .then(() => console.log('Connected to DB!'))
 .catch(error => console.log(error.message));
-
-
-
-
-
-
-
-// Campground.create(
-// 	{
-// 		name: "Granite Hill",
-// 		image: "https://images.unsplash.com/photo-1528433556524-74e7e3bfa599?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-// 		description: "A complete granite hill, which will make you feel awesome!"
-// 	}
-// 	,function(err,campground)
-// 	{
-// 		if(err) console.log(err);
-// 		else
-// 		{
-// 			console.log("NEW CAMPGROUND CREATED");
-// 			console.log(campground);
-// 		}
-// 	});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
